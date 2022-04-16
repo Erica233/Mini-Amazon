16d47f79c933c4f5a48fcc70b49fb6f17f862a91
Fangting Ma <fangtingm@student.unimelb.edu.au>
2022-04-16 11:38:10 -0400

    setting

--- frontend/frontend/settings.py
+++ frontend/frontend/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'amazon',
         'USER': 'postgres',
-        'PASSWORD': 'postgres',
+        'PASSWORD': 'passw0rd',
         'HOST': '127.0.0.1',
         'PORT': 5432,
     }
