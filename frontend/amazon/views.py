63978b9c5dc388a6710704c5a2dba711f3d61a70
Fangting Ma <fangtingm@student.unimelb.edu.au>
2022-04-17 09:15:59 -0400

    home

--- frontend/amazon/views.py
+++ frontend/amazon/views.py
@@ -4,5 +4,8 @@ from django.http import HttpResponse
 def home(request):
     return HttpResponse("<h1>Amazon Home</h1>")
 
+def about(request):
+    return HttpResponse("<h1>Amazon About</h1>")
+
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
