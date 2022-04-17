fa1d0b9732f7def4994ec19010170fdb6b529560
Fangting Ma <fangtingm@student.unimelb.edu.au>
2022-04-17 09:41:55 -0400

    home

--- frontend/amazon/views.py
+++ frontend/amazon/views.py
@@ -1,8 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+products = [
+    {
+        'name': 'apple',
+        'price': '13',
+        'seller': 'Erica'
+    },
+    {
+        'name': 'pen',
+        'price': '300',
+        'seller': 'Beta'
+    }
+]
+
 def home(request):
-    return render(request, 'amazon/home.html')
+    context = {
+        'products': products
+    }
+    return render(request, 'amazon/home.html', context)
 
 def about(request):
     return render(request, 'amazon/about.html')
