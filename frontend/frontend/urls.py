a1c4b7d6c151eebc80a7c4788dc50ca9ff656739
Fangting Ma <fangtingm@student.unimelb.edu.au>
2022-04-17 09:22:05 -0400

    home

--- frontend/frontend/urls.py
+++ frontend/frontend/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('amazon/', include('amazon.urls')),
+    path('', include('amazon.urls')),
 ]
