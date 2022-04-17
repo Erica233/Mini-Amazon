ab5fd2f4794fc13e8a97b923205a751db9dac83a
Fangting Ma <fm128@duke.edu>
2022-04-16 21:06:45 -0400

    rm

--- frontend/amazon/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def home(request):
-    return HttpResponse('<h1>Amazon Home</h1>')
-
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
