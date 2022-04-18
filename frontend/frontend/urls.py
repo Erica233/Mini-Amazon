85e7259ebccd4b7c195cc4bbd65b096a907b7b8b
Fangting Ma <fangtingm@student.unimelb.edu.au>
2022-04-18 19:23:23 -0400

    user-account

--- frontend/frontend/urls.py
+++ frontend/frontend/urls.py
@@ -22,6 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('amazon.urls')),
     path('register/', user_views.register, name='register'),
-    path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('login/', auth_views.LoginView.as_view(template_name='user/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='user/logout.html'), name='logout'),
 ]
