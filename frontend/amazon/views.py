fcc05fa4040da35d70a9e5de687d25384dfa8899
Fangting Ma <fangtingm@student.unimelb.edu.au>
2022-04-17 23:58:16 -0400

    model - simple

--- frontend/amazon/views.py
+++ frontend/amazon/views.py
@@ -2,19 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Item
 
-products = [
-    {
-        'name': 'apple',
-        'price': '13',
-        'seller': 'Erica'
-    },
-    {
-        'name': 'pen',
-        'price': '300',
-        'seller': 'Beta'
-    }
-]
-
 def home(request):
     context = {
         'products': Item.objects.all()
