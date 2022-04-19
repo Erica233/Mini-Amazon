50b54729d4075d20e9c86461f964d66150ce7aec
Fangting Ma <fangtingm@student.unimelb.edu.au>
2022-04-19 15:53:48 -0400

    product

--- frontend/amazon/admin.py
+++ frontend/amazon/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import Item, Category, Product, Warehouse, Package
 
-admin.site.register(Item, Category, Product, Warehouse, Package)
+admin.site.register(Item)
+admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(Warehouse)
+admin.site.register(Package)
