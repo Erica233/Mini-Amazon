81657f1d7aaa68c6f5dee696978e34e02892927e
Fangting Ma <fangtingm@student.unimelb.edu.au>
2022-04-19 15:00:33 -0400

    myInfo

--- frontend/user/views.py
+++ frontend/user/views.py
@@ -3,6 +3,7 @@ from .forms import UserRegisterForm
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
+
 def register(request):
     if request.method == 'POST':
         user = UserRegisterForm(request.POST)
