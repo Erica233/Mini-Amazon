c980c28be5dbfc2eda3b7e4c6cdf26d90d68c7ca
Fangting Ma <fangtingm@student.unimelb.edu.au>
2022-04-19 17:59:46 -0400

    product - category

--- frontend/amazon/views.py
+++ frontend/amazon/views.py
@@ -1,16 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Item, Product
+from .models import Item, Product, Category
 #from django.views.generic import ListView
 
 def home(request):
     context = {
-        'products': Product.objects.all()
+        'categories': Category.objects.all()
     }
     return render(request, 'amazon/home.html', context)
 
 def about(request):
     return render(request, 'amazon/about.html', {'title': 'About'})
 
-'''def category(request):
-    return render(request, 'amazon/category.html', {'category': ''})'''
+def category(request):
+    context = {
+        'categories': Category.objects.all()
+    }
+    return render(request, 'amazon/category.html', context)
+
