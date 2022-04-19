5ab5ec74f9ce0d41971248421c139222fe63c307
Fangting Ma <fangtingm@student.unimelb.edu.au>
2022-04-19 13:00:51 -0400

    myInfo

--- frontend/user/models.py
+++ frontend/user/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Profile(models.Model):
+class MyInfo(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     upsAccount = models.CharField(max_length=50, null=True)
 
