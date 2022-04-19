7e0e50adb640b0ea74116e2e3ac9f46cabded7a8
Fangting Ma <fangtingm@student.unimelb.edu.au>
2022-04-19 18:24:35 -0400

    product - category

--- frontend/amazon/views.py
+++ frontend/amazon/views.py
@@ -12,10 +12,14 @@ def home(request):
 def about(request):
     return render(request, 'amazon/about.html', {'title': 'About'})
 
-def products(request):
+def products(request, a_category):
+    if a_category:
+        products = Product.object.filter(category_category=a_category)
+    else:
+        products = Product.object.all()
     context = {
         'categories': Category.objects.all(),
-        'products': Product.objects.all()
+        'products': products
     }
     return render(request, 'amazon/products.html', context)
 
